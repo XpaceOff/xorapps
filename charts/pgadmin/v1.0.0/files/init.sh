05b69858c884d229847653d75fe767cf6717b259
xoffio <marquessricardo@gmail.com>
2024-04-06 23:54:08 -0400

    bug and permission issues fixed

--- charts/pgadmin/v1.0.0/files/init.sh
+++ charts/pgadmin/v1.0.0/files/init.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
-
+echo "Running init.sh..."
 pgadmin_dir="/tmp_mount/$1/pgadmin"
 
-if [ ! -d "$pgadmin_dir" ]; then
-	chown -R pgadminr:root "$pgadmin_dir/*"
+if [ -d "$pgadmin_dir" ]; then
+	echo "Permission before:"
+	ls -l "$pgadmin_dir/"
+
+	echo ""
+	echo "Setting the right owner..."
+	chown -R 5050:root "$pgadmin_dir/"
+	chmod 777 "$pgadmin_dir"
+
+	echo ""
+	echo "Permission after:"
+	ls -l "$pgadmin_dir/"
 fi
