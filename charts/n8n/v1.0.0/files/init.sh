c2297f98d796fec7e2fbf16977475a2d78129d07
xoffio <marquessricardo@gmail.com>
2024-06-09 19:30:13 -0400

    i think this is ready. needs testing.

---
+++ charts/n8n/v1.0.0/files/init.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+echo "Running init.sh..."
+n8n_dir="/tmp_mount/$1/n8n"
+
+if [ -d "$n8n_dir" ]; then
+	echo "Permission before:"
+	ls -l "$n8n_dir/"
+
+	echo ""
+	echo "Setting the right owner..."
+	# chown -R 5050:root "$n8n_dir/"
+	# chmod 777 "$n8n_dir"
+
+	echo ""
+	echo "Permission after:"
+	ls -l "$n8n_dir/"
+fi
